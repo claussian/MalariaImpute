15331022d81f3cd21ceb5d56c88684babd7f635c
claussian <hanif.samad.sg@gmail.com>
2014-02-09 15:58:29 +0800

    EHH scan

---
+++ iES_bgl_fullscan_writer.R
@@ -0,0 +1,75 @@
+script.pa <- "library(rehh)
+
+haps.pa <- data2haplohh('chrZZ_pa_bglXX.rehh','markerZZ_rehh.txt',chr.name=ZZ)
+scan <- scan_hh(haps.pa)
+
+write.table(scan, 'iES_chrZZ_pa_bglXX.txt', sep='\\t',col.names=T, row.names=F, quote=F)"
+
+script.pd <- "library(rehh)
+
+haps.pd <- data2haplohh('chrZZ_pd_bglXX.rehh','markerZZ_rehh.txt',chr.name=ZZ)
+scan <- scan_hh(haps.pd)
+
+write.table(scan, 'iES_chrZZ_pd_bglXX.txt', sep='\\t',col.names=T, row.names=F, quote=F)"
+
+script.ph <- "library(rehh)
+
+haps.ph <- data2haplohh('chrZZ_ph_bglXX.rehh','markerZZ_rehh.txt',chr.name=ZZ)
+scan <- scan_hh(haps.ph)
+
+write.table(scan, 'iES_chrZZ_ph_bglXX.txt', sep='\\t',col.names=T, row.names=F, quote=F)"
+
+
+script.ph <- "library(rehh)
+
+haps.pt <- data2haplohh('chrZZ_pt_bglXX.rehh','markerZZ_rehh.txt',chr.name=ZZ)
+scan <- scan_hh(haps.pt)
+
+write.table(scan, 'iES_chrZZ_pt_bglXX.txt', sep='\\t',col.names=T, row.names=F, quote=F)"
+
+
+script.pv <- "library(rehh)
+
+haps.pv <- data2haplohh('chrZZ_pv_bglXX.rehh','markerZZ_rehh.txt',chr.name=ZZ)
+scan <- scan_hh(haps.pv)
+
+write.table(scan, 'iES_chrZZ_pv_bglXX.txt', sep='\\t',col.names=T, row.names=F, quote=F)"
+
+
+scriptlist.pa <- list()
+scriptlist.pd <- list()
+scriptlist.ph <- list()
+scriptlist.pt <- list()
+scriptlist.pv <- list()
+
+for (i in 1:14)
+{scriptlist.pa[[i]] <- gsub('ZZ', i, script.pa)
+scriptlist.pd[[i]] <- gsub('ZZ', i, script.pd)
+scriptlist.ph[[i]] <- gsub('ZZ', i, script.ph)
+scriptlist.pt[[i]] <- gsub('ZZ', i, script.pt)
+scriptlist.pv[[i]] <- gsub('ZZ', i, script.pv)
+shell.pa <- list()
+shell.pd <- list()
+shell.ph <- list()
+shell.pt <- list()
+shell.pv <- list()
+
+	for (j in 1:10)
+	{shell.pa[[j]] <- gsub('XX', j, scriptlist.pa[[i]])
+	shell.pd[[j]] <- gsub('XX', j, scriptlist.pd[[i]])
+	shell.ph[[j]] <- gsub('XX', j, scriptlist.ph[[i]])
+	shell.pt[[j]] <- gsub('XX', j, scriptlist.pt[[i]])
+	shell.pv[[j]] <- gsub('XX', j, scriptlist.pv[[i]])
+	write.table(shell.pa[[j]], paste('iES_chr',i,'_pa_bgl',j,'.R',sep=''),col.names=F, row.names=F, quote=F)
+	write.table(shell.pd[[j]], paste('iES_chr',i,'_pd_bgl',j,'.R',sep=''),col.names=F, row.names=F, quote=F)
+	write.table(shell.ph[[j]], paste('iES_chr',i,'_ph_bgl',j,'.R',sep=''),col.names=F, row.names=F, quote=F)
+	write.table(shell.pt[[j]], paste('iES_chr',i,'_pt_bgl',j,'.R',sep=''),col.names=F, row.names=F, quote=F)
+	write.table(shell.pv[[j]], paste('iES_chr',i,'_pv_bgl',j,'.R',sep=''),col.names=F, row.names=F, quote=F)
+	}
+}
+
+
+
+
+
+
---
+++ multiple_imputation_sampler_bgl_writer.R
@@ -0,0 +1,64 @@
+script <- "pa.imp <- read.table('Full.bgl_gprobs_chrZZ_pa', header=F)
+pd.imp <- read.table('Full.bgl_gprobs_chrZZ_pd', header=F)
+ph.imp <- read.table('Full.bgl_gprobs_chrZZ_ph', header=F)
+pt.imp <- read.table('Full.bgl_gprobs_chrZZ_pt', header=F)
+pv.imp <- read.table('Full.bgl_gprobs_chrZZ_pv', header=F)
+samples <- readLines('5pop_590.samples')
+
+#sample 10 datasets according to posterior probabilities, vector(1,2) for major and minor alleles
+allele.sampler = function(pop) {
+	imp <- sapply(pop,function(x) {sample(c(1,2),1,prob=c(x,1-x))})
+return (imp)
+}
+
+pa.mi <- list()
+pd.mi <- list()
+ph.mi <- list()
+pt.mi <- list()
+pv.mi <- list()
+
+pa.rehh <- list()
+pd.rehh <- list()
+ph.rehh <- list()
+pt.rehh <- list()
+pv.rehh <- list()
+
+for (i in 1:10)
+{pa.mi[[i]] <- data.frame(t(apply(pa.imp,1,allele.sampler)))
+pd.mi[[i]] <- data.frame(t(apply(pd.imp,1,allele.sampler)))
+ph.mi[[i]] <- data.frame(t(apply(ph.imp,1,allele.sampler)))
+pt.mi[[i]] <- data.frame(t(apply(pd.imp,1,allele.sampler)))
+pv.mi[[i]] <- data.frame(t(apply(ph.imp,1,allele.sampler)))
+
+colnames(pa.mi[[i]]) <- as.character(samples[405:459])
+colnames(pd.mi[[i]]) <- as.character(samples[1:91])
+colnames(ph.mi[[i]]) <- as.character(samples[92:344])
+colnames(pt.mi[[i]]) <- as.character(samples[345:404])
+colnames(pv.mi[[i]]) <- as.character(samples[460:590])
+
+pa.rehh[[i]] <- t(pa.mi[[i]])
+pd.rehh[[i]] <- t(pd.mi[[i]])
+ph.rehh[[i]] <- t(ph.mi[[i]])
+pt.rehh[[i]] <- t(pt.mi[[i]])
+pv.rehh[[i]] <- t(pv.mi[[i]])
+
+write.table(pa.rehh[[i]], file = paste('chrZZ_pa_bgl',i,'.rehh',sep=''), 
+	sep='\t', quote =F, col.names=F, row.names=T)
+write.table(pd.rehh[[i]], file = paste('chrZZ_pd_bgl',i,'.rehh',sep=''), 
+	sep='\t', quote =F, col.names=F, row.names=T)
+write.table(ph.rehh[[i]], file = paste('chrZZ_ph_bgl',i,'.rehh',sep=''), 
+	sep='\t', quote =F, col.names=F, row.names=T)
+write.table(pt.rehh[[i]], file = paste('chrZZ_pt_bgl',i,'.rehh',sep=''), 
+	sep='\t', quote =F, col.names=F, row.names=T)
+write.table(pv.rehh[[i]], file = paste('chrZZ_pv_bgl',i,'.rehh',sep=''), 
+	sep='\t', quote =F, col.names=F, row.names=T)
+}"
+
+scriptlist <- list()
+
+for (i in 1:14)
+{scriptlist[[i]] <- gsub('ZZ', i, script)
+write.table(scriptlist[[i]], paste('MI_bgl_sampler_chr',i,'.R',sep=''),col.names=F, row.names=F, quote=F)
+}
+	
+
